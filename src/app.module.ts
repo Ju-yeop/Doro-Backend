80aad28739122b0ca9127453d9f97000bc8adaa2
Ju-yeop <dhwnduq12@naver.com>
2022-09-01 12:59:59 +0900

    Synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,7 +47,7 @@ import { AuthModule } from './auth/auth.module';
             database: process.env.DB_NAME,
           })),
 
-      synchronize: true,
+      synchronize: false,
       logging: true,
       entities: [User, Post, Comment],
     }),
